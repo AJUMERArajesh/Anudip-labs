11b9222ec3271f121b81bca452369c5be499269f
AJUMERArajesh <ajumerarajesh@gmail.com>
2024-07-31 18:45:31 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,20 @@
+package NEW_PROGRAMS;
+
+public class Rectangle {
+		    double length;
+		    double width;
+
+		    void calculateArea() {
+		        double area = this.length * this.width;
+		        System.out.println("Area of rectangle is: " + area);
+		    }
+
+		    void calculatePerimeter() {
+		        double perimeter = 2 * (this.length + this.width);
+		        System.out.println("Perimeter of rectangle is: " + perimeter);
+		    }
+		}
+
+		
+
+
