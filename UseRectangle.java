24869cdd400f893030db2ade3c5a63fd926d5b2a
AJUMERArajesh <ajumerarajesh@gmail.com>
2024-07-31 18:45:56 +0530

    Add files via upload

---
+++ UseRectangle.java
@@ -0,0 +1,16 @@
+package NEW_PROGRAMS;
+
+class UseRectangle {
+    public static void main(String[] args) {
+        Rectangle rect = new Rectangle();
+        rect.length = 15.854;
+        rect.width = 22.65;
+
+        System.out.println("Length = " + rect.length);
+        System.out.println("Width = " + rect.width);
+
+        rect.calculateArea();
+        rect.calculatePerimeter();
+    }
+
+}
